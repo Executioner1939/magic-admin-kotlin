87d56b5bda567affb3593424b3aaebeeb756c9d3
Richard <shadowrhyder@gmail.com>
2024-11-26 14:29:14 +0200

    update: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.web3j:core:4.12.2")
-    api("io.ktor:ktor-client-core:2.3.7")
-    api("io.ktor:ktor-client-java:2.3.7")
-    api("io.ktor:ktor-client-content-negotiation:2.3.7")
-    api("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.web3j:core:5.0.0")
+    api("io.ktor:ktor-client-core:3.0.1")
+    api("io.ktor:ktor-client-java:3.0.1")
+    api("io.ktor:ktor-client-content-negotiation:3.0.1")
+    api("io.ktor:ktor-serialization-kotlinx-json:3.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.+")
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-client-mock:2.3.7")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("io.ktor:ktor-client-mock:3.0.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.10")
-    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("io.mockk:mockk:1.13.13")
 }
 
 tasks.test {
